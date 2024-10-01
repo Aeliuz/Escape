9c6d1e3e63e55d469b82356584328b29a01301e3
Johan Lamberth <johan.lamberth@gmail.com>
2024-10-01 14:38:43 +0200

    room scaling

--- Assets/ScaleRoom.cs
+++ Assets/ScaleRoom.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ScaleRoom : MonoBehaviour
 {
@@ -11,17 +12,33 @@ public class ScaleRoom : MonoBehaviour
     public Transform room;
     public Transform eyePosition;
 
-    // Start is called before the first frame update
+    float originalScale = 1.6f;
+    float originalHeight = 1.82f;
+    float heightToEyeHeight = 1.07f;
+
+
+
     void Start()
     {
-        // roomScaleCoefficient = eyePosition.position.y;
-
-        room.localScale = new Vector3 (roomScale, roomScale, roomScale);
+        StartCoroutine(Resize());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator Resize()
     {
-        
+        yield return new WaitForSeconds(1f);
+
+        roomScaleCoefficient = eyePosition.position.y;
+
+        float newScaleCoefficient = (roomScaleCoefficient * heightToEyeHeight) / originalHeight;
+        float newScale = originalScale * newScaleCoefficient;
+
+        room.localScale = new Vector3 (newScale, newScale, newScale);
+
+        Debug.Log("1: " + roomScaleCoefficient + ", 2: " + newScaleCoefficient + ", 3: " + newScale, eyePosition);
+    }
+
+    public void RestartScene ()
+    {
+        SceneManager.LoadScene(0);
     }
 }
