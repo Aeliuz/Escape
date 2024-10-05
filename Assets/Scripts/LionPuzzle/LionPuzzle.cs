812b3fe33c347df7f340229caaa5d7389bbc15d8
B0realian <142990172+B0realian@users.noreply.github.com>
2024-10-06 00:19:52 +0200

    Created LionPuzzle (almost working too)

--- Assets/Scripts/LionPuzzle/LionPuzzle.cs
+++ Assets/Scripts/LionPuzzle/LionPuzzle.cs
@@ -1,18 +1,41 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class LionPuzzle : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private static List<Lion> lions = new List<Lion>();
+    public static event Action OnLionPuzzleComplete;
+
+    private void Start()
     {
-        
+        foreach (GameObject lion in GameObject.FindGameObjectsWithTag("Lion"))
+        {
+            lions.Add(lion.GetComponent<Lion>());
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void LionUpdate()        // This function is called by a Unity event whenever a lion is snapped to a pedestal
     {
-        
+        Debug.LogWarning("Lion snapped!");
+        if (AllLionsInPlace())
+        {
+            foreach (Lion lion in lions)
+            {
+                lion.transform.GetChild(2).gameObject.SetActive(false);
+                lion.transform.GetChild(0).gameObject.SetActive(false);
+            }
+            OnLionPuzzleComplete?.Invoke();
+        }
+    }
+
+    private static bool AllLionsInPlace()
+    {
+        foreach(Lion lion in lions)
+        {
+            if (!lion.inPosition()) return false;
+        }
+        return true;
     }
 }
