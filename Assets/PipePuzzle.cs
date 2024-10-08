9f22d9b3335f6860e04e2f401d16004a32fdde51
Johan Lamberth <johan.lamberth@gmail.com>
2024-10-08 11:29:50 +0200

    update

--- Assets/PipePuzzle.cs
+++ Assets/PipePuzzle.cs
@@ -1,11 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using Oculus.Interaction;
 using UnityEngine;
 
 public class PipePuzzle : MonoBehaviour
 {
+
+    public GameObject ball;
+
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log("Pipe Puzzle Complete");
+        ball.GetComponent<SnapInteractor>().InjectOptionaTimeOut(-1);
     }
 }
