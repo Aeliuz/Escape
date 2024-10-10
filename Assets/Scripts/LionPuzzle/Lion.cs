d4329e8481eb0c0a3472f72842b4bf848574d378
B0realian <142990172+B0realian@users.noreply.github.com>
2024-10-10 16:42:32 +0200

    Update Lion.cs

--- Assets/Scripts/LionPuzzle/Lion.cs
+++ Assets/Scripts/LionPuzzle/Lion.cs
@@ -5,7 +5,7 @@ public class Lion : MonoBehaviour
 {
     public bool inPosition()
     {
-        return (Math.Abs(this.gameObject.transform.localPosition.x) < 0.05f &&
-                Math.Abs(this.gameObject.transform.localPosition.z) < 0.05f);
+        return (Math.Abs(this.gameObject.transform.localPosition.x) < 0.08f &&
+                Math.Abs(this.gameObject.transform.localPosition.z) < 0.08f);
     }
 }
